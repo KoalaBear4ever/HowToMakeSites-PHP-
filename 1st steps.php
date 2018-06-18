58e346be63c97e79a58059e36db6f7ab2d0ae06c
KoalaBear4ever <mex.mistik@mail.ru>
2018-06-18 23:31:38 +0600

    bug fix

---
+++ 1st steps.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>
+        Первая проба пера в PhpStorm!
+    </title>
+</head>
+<body>
+<?php
+// # Настройка PhpStorm(базовая) и подключение к нему Xampp
+// # {Click Me}[https://rutracker.org/forum/viewtopic.php?t=5431405]
+// # {Click Me}[https://www.imsxm.com/jetbrains-license-server.html]
+// # {Click Me}[http://www.codetuple.com/articles/jetbrains/N4zMrFhF5ma/phpstorm-simple-setting-up-web-server-xampp-and-amp-wamp-1/]
+    echo '<p>Приветик :)</p>';
+?>
+</body>
+</html>
