881478bee0eb430d4c5f9f984d2d9eee4f8e18dd
KoalaBear4ever <mrx.mistik@mail.ru>
2018-06-22 14:41:46 +0600

    bug fix

--- 1st.steps/3les.php
+++ 1st.steps/3les.php
@@ -23,6 +23,7 @@
 	$x = 1;
 	echo $x + '2';
 	echo '<br>';
+
 	# В ручную переводим типы по формуле (тип)значение
 	$string = '123';                        // Здесь строка
 	$numeric = (int) $string;               // Переделали в целочисленный тип
@@ -30,29 +31,3 @@
 	# Тип Boolean
 	$x = true;
 	$y = false;
-
-	# ДЗ
-	echo 'ДЗ <br>';
-	var_dump(!1);
-	echo '<br>';
-	var_dump(!0);
-	echo '<br>';
-	var_dump(!true);
-	echo '<br>';
-	var_dump(2 && 3);
-	echo '<br>';
-	var_dump(5 && 0);
-	echo '<br>';
-	var_dump(3 || 0);
-	echo '<br>';
-	var_dump(5 / 1);
-	echo '<br>';
-	var_dump(1 / 5 );
-	echo '<br>';
-	var_dump(5 + '5string');
-	echo '<br>';
-	var_dump('5' == 5);
-	echo '<br>';
-	var_dump('05' == 5);
-	echo '<br>';
-	var_dump('05' == '5');
